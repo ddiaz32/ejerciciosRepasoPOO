2388bd52757ffb2849cef098588aa27903a80a7a
ddiaz32 <ddiaz@vertical.com.co>
2023-02-21 17:09:16 -0500

    Sexto ejercicio completado

---
+++ exercise_6.py
@@ -0,0 +1,15 @@
+#Crear un programa que calcule la suma de los números en una lista dada.
+
+def sumaLista(list):
+    suma = 0 
+    for number in list:
+        suma += number 
+    return suma 
+
+
+list=[5,10,15,20,25]
+
+sumaT=sumaLista(list)
+
+print("La suma total es:",sumaT)
+    
