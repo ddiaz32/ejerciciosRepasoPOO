6632854d557a4c0df8a93649c2dd2794ff710b33
ddiaz32 <ddiaz@vertical.com.co>
2023-02-21 15:08:50 -0500

    Actualizated

---
+++ exercise_1.py
@@ -0,0 +1,10 @@
+#Crear un programa que pida al usuario ingresar su nombre y edad y los imprima en pantalla.
+
+nombre = input("Ingrese su nombre : ")
+edad = input("Ingrese su edad : ")
+
+print("Su nombre es:", nombre , 
+       "Su edad es: ", edad )
+
+
+
