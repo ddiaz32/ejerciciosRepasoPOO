d52de39ab5a5d3c87bef9cb522177ae1a71d5a46
ddiaz32 <ddiaz@vertical.com.co>
2023-02-21 15:36:18 -0500

    Segundo ejercico resuelto

---
+++ exercise_2.py
@@ -0,0 +1,13 @@
+#Escribir una función que calcule el área de un círculo dado su radio.
+import math
+
+
+def calculaRadio(radio):
+    pi = 3.1416
+    area = pi * (radio ** 2)
+    return area
+
+
+
+radio = calculaRadio(int(input("Ingrese el radio del area a calcular:")))
+print(radio)
