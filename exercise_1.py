e8b579cc5e5dc78e3fbf260e94b3301f07773b25
ddiaz32 <ddiaz@vertical.com.co>
2023-02-21 20:44:29 -0500

    Sexto ejercicio con negrilla xd

--- exercise_1.py
+++
@@ -1,8 +0,0 @@
-#Crear un programa que pida al usuario ingresar su nombre y edad y los imprima en pantalla.
-
-nombre = input("Ingrese su nombre : ")
-edad = input("Ingrese su edad : ")
-
-print("Su nombre es:", nombre ,
-       "Su edad es: ", edad )
-
