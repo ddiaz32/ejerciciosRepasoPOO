71ad603311bb755ac155dea95f3ce38168ddac9f
ddiaz32 <ddiaz@vertical.com.co>
2023-02-21 22:33:22 -0500

    Cuarto exercise corregido

--- exercise_4.py
+++ exercise_4.py
@@ -1,8 +1,14 @@
 #Escribir un programa que determine si un número dado es par o impar.
 
-numero = int(input("Ingrese un numero para analizar: "))
 
-if numero % 2 == 0 :
-    print("El numero es par")
-else:
-    print("El numero es impar")
+def verNumPar(numero):
+    if numero % 2 == 0 :
+        print("El numero es par")
+    else:
+        print("El numero es impar")
+
+numero = verNumPar(int(input("Ingrese un numero para analizar: ")))
+
+        
+        
+
---
+++ exercise_7.py
@@ -0,0 +1,15 @@
+#Escribir un programa que encuentre el número más grande y el más pequeño en una lista dada.
+
+import random 
+
+list=[]
+
+n=int(input("Ingrese el numero de elementos que desea en la lista:"))
+
+for i in range(n):
+    num=random.randint(1:100)
+    list+=num
+
+
+
+print(list)
